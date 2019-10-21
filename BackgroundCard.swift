b6829fb410609f28d648e2ecf705468e3e880e6e
Matt Bonney <bonneym@gmail.com>
2019-10-21 11:40:12 -0400

    Created BackgroundCard.swift

---
+++ BackgroundCard.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+struct BackgroundCard: ViewModifier {
+    var radius: CGFloat = 12
+    var border: CGFloat {
+        // TODO: Increase border size slighty if Bold Text is enabled
+        return 1
+    }
+    var shadow: Double = 0.03
+    
+    func body(content: Content) -> some View {
+        content
+            .padding()                  // Padding around the edges of content
+            .background(innerRect)      // White|Black background color
+            .padding(border)            // Make space for border rectangle
+            .background(outerRect)      // Dark|Light gray border rectangle
+            .shadow(color: Color.black.opacity(shadow), radius: 4, x: 0, y: 4)
+    }
+    
+    var innerRect: some View {
+        RoundedRectangle(cornerRadius: radius, style: .continuous)
+            .foregroundColor(Color(UIColor.secondarySystemGroupedBackground))
+    }
+    
+    var outerRect: some View {
+        RoundedRectangle(cornerRadius: (radius + border), style: .continuous)
+            .foregroundColor(Color(UIColor.systemGray5))
+    }
+}
+
+struct BackgroundCard_Previews: PreviewProvider {
+	static var previews: some View {
+		Text("Hi there! I hope you find this simple ViewModifier useful."
+			.modifier(BackgroundCard())
+	}
+}
